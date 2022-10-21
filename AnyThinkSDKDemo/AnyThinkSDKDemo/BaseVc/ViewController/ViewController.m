8bb4448c7dfea3ab8fa74114785f474e49d1704a
lix <>
2022-10-21 18:56:34 +0800

    v5.9.98  添加自定义广告平台adapter的Demo，参考CustomAdapter文件

--- AnyThinkSDKDemo/AnyThinkSDKDemo/BaseVc/ViewController/ViewController.m
+++ AnyThinkSDKDemo/AnyThinkSDKDemo/BaseVc/ViewController/ViewController.m
@@ -88,7 +88,13 @@
             @"title":@"Native",
             @"class":@"ATNativeMainViewController",
             @"des":@"Include Native,Vertical Draw Video and Pre-roll Ads.Most compatible with your native app code for video ads and graphic ads.",
-        }
+        },
+        @{
+            @"image":@"splash",
+            @"title":@"CustomAdapterSplash",
+            @"class":@"ATTMCustomSplashViewController",
+            @"des":@"Displayed immediately after the application is launched.",
+        },
     ];
 }
 
---
+++ AnyThinkSDKDemo/AnyThinkSDKDemo/CustomAdapter/CustomSplash/ATTMBiddingDelegate.m
@@ -0,0 +1,66 @@
+//
+//  ATTMBiddingDelegate.m
+//  HeadBiddingDemo
+//
+//  Created by lix on 2022/10/20.
+//
+
+#import "ATTMBiddingDelegate.h"
+#import "ATTMBiddingManager.h"
+#import "ATTMBiddingRequest.h"
+
+#import <TianmuSDK/TianmuSDK.h>
+#import <TianmuSDK/TianmuSplashAd.h>
+#import <AnyThinkSplash/AnyThinkSplash.h>
+
+@interface ATTMBiddingDelegate () <TianmuSplashAdDelegate>
+
+@end
+
+@implementation ATTMBiddingDelegate
+
+
+- (void)tianmuSplashAdSuccessLoad:(TianmuSplashAd *)splashAd{
+    NSLog(@"%s", __FUNCTION__);
+}
+
+/**
+ *  开屏广告素材加载成功
+ */
+- (void)tianmuSplashAdDidLoad:(TianmuSplashAd *)splashAd {
+    
+    
+    NSLog(@"%s", __FUNCTION__);
+
+    ATTMBiddingRequest *request = [[ATTMBiddingManager sharedInstance] getRequestItemWithUnitID:self.unitID];
+    
+    if (request.bidCompletion) {
+        ATBidInfo *bidInfo = [ATBidInfo bidInfoWithPlacementID:request.placementID
+                                               unitGroupUnitID:request.unitGroup.unitID
+                                            adapterClassString:request.unitGroup.adapterClassString
+                                                         token:request.unitGroup.content[@"unitid"]
+                                                         price:@(splashAd.bidPrice/100.0f).stringValue
+                                                  currencyType:ATBiddingCurrencyTypeCNY
+                                            expirationInterval:request.unitGroup.networkTimeout
+                                                  customObject:splashAd];
+        request.bidCompletion(bidInfo, nil);
+    }
+    
+    [[ATTMBiddingManager sharedInstance] removeBiddingDelegateWithUnitID:self.unitID];
+    
+}
+
+/**
+ *  开屏广告请求失败
+ */
+- (void)tianmuSplashAdFailLoad:(TianmuSplashAd *)splashAd withError:(NSError *)error{
+    NSLog(@"%s %@", __FUNCTION__ ,error);
+    
+    ATTMBiddingRequest *request = [[ATTMBiddingManager sharedInstance] getRequestItemWithUnitID:self.unitID];
+    if (request.bidCompletion) {
+        request.bidCompletion(nil, error);
+    }
+    [[ATTMBiddingManager sharedInstance] removeBiddingDelegateWithUnitID:self.unitID];
+}
+
+@end
