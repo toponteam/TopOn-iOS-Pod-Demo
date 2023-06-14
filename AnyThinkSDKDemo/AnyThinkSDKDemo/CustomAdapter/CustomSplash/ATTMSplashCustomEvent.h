2370a8f6c4f775d190681899dbbea030edb96af3
lix <>
2023-06-14 15:10:09 +0800

    v6.2.16  适配新版本的自定义广告平台的竞价接入方式。
    (cherry picked from commit 379f91c89d5daaf7a604734f045c518877516ab0)

--- AnyThinkSDKDemo/AnyThinkSDKDemo/CustomAdapter/CustomSplash/ATTMSplashCustomEvent.h
+++ AnyThinkSDKDemo/AnyThinkSDKDemo/CustomAdapter/CustomSplash/ATTMSplashCustomEvent.h
@@ -15,15 +15,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ATTMSplashCustomEvent : ATSplashCustomEvent <TianmuSplashAdDelegate>
+
 @property(nonatomic, weak) UIView *containerView;
-@property(nonatomic, weak) UIImageView *backgroundImageView;
-
-@property(nonatomic, copy) NSString *price;
-@property(nonatomic, copy) NSString *bidID;
-@property(nonatomic, weak) ATPlacementModel *placementModel;
-@property(nonatomic, weak) ATUnitGroupModel *unitGroupModel;
-@property(nonatomic, copy) void(^BidCompletionBlock)(ATBidInfo *bidInfo, NSError *error);
-
 
 @end
 
