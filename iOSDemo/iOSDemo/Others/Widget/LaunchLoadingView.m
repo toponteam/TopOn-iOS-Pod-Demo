39eb4d111c2732490a2166d7f90c300b18243bca
ling <tianzi.ling@toponad.com>
2025-06-26 15:30:59 +0800

    edit timer

--- iOSDemo/iOSDemo/Others/Widget/LaunchLoadingView.m
+++ iOSDemo/iOSDemo/Others/Widget/LaunchLoadingView.m
@@ -11,8 +11,9 @@
 @interface LaunchLoadingView()
 
 @property (nonatomic, strong) UILabel *timerLabel;
-@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, strong) dispatch_source_t timer;
 @property (nonatomic, assign) NSInteger seconds;
+@property (nonatomic, strong) dispatch_queue_t timerQueue;
 
 @end
 
@@ -21,6 +22,9 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+        // 创建专用的串行队列用于计时器
+        self.timerQueue = dispatch_queue_create("com.launchloadingview.timer", DISPATCH_QUEUE_SERIAL);
+        
         // 设置背景色为透明或者其他颜色
         self.backgroundColor = [UIColor whiteColor]; // 半透明黑色，根据需求调整
         
@@ -65,34 +69,69 @@
 }
 
 - (void)startTimer {
-    
-    if (self.timer) {
-        [self.timer invalidate];
-        self.timer = nil;
-    }
+    // 先停止之前的计时器
+    [self stopTimer];
     
     // 重置计时器
     self.seconds = 0;
-    self.timerLabel.text = [NSString stringWithFormat:@"%@ 00:00 ,%@:%d",kLocalizeStr(@"当前"),kLocalizeStr(@"总超时时间"),FirstAppOpen_Timeout];
+    
+    // 在主线程更新UI
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.timerLabel.text = [NSString stringWithFormat:@"%@ 00:00 ,%@:%d",kLocalizeStr(@"当前"),kLocalizeStr(@"总超时时间"),FirstAppOpen_Timeout];
+    });
 
-    // 启动计时器
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+    // 创建高精度计时器
+    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, self.timerQueue);
+    
+    if (self.timer) {
+        // 设置计时器：立即开始，每1秒触发一次，允许0.1秒的误差
+        dispatch_source_set_timer(self.timer,
+                                 dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC),
+                                 1.0 * NSEC_PER_SEC,
+                                 0.1 * NSEC_PER_SEC);
+        
+        // 设置计时器事件处理
+        __weak typeof(self) weakSelf = self;
+        dispatch_source_set_event_handler(self.timer, ^{
+            __strong typeof(weakSelf) strongSelf = weakSelf;
+            if (strongSelf) {
+                [strongSelf updateTimer];
+            }
+        });
+        
+        // 启动计时器
+        dispatch_resume(self.timer);
+    }
 }
 
 - (void)updateTimer {
     self.seconds++;
     NSInteger minutes = self.seconds / 60;
     NSInteger remainingSeconds = self.seconds % 60;
-    self.timerLabel.text = [NSString stringWithFormat:@"%@ %02ld:%02ld ,%@:%d",kLocalizeStr(@"当前") ,(long)minutes, (long)remainingSeconds,kLocalizeStr(@"总超时时间"),FirstAppOpen_Timeout];
+    
+    // 确保UI更新在主线程执行
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.timerLabel.text = [NSString stringWithFormat:@"%@ %02ld:%02ld ,%@:%d",kLocalizeStr(@"当前") ,(long)minutes, (long)remainingSeconds,kLocalizeStr(@"总超时时间"),FirstAppOpen_Timeout];
+    });
+}
+
+- (void)stopTimer {
+    if (self.timer) {
+        dispatch_source_cancel(self.timer);
+        self.timer = nil;
+    }
 }
 
 - (void)dismiss {
-    // 如果有需要清理的资源或者引用，在这里进行
     // 停止计时器
-    [self.timer invalidate];
-    self.timer = nil;
+    [self stopTimer];
     
     [self removeFromSuperview]; // 从父视图中移除
+}
+
+- (void)dealloc {
+    // 确保在对象销毁时停止计时器
+    [self stopTimer];
 }
 
 @end
