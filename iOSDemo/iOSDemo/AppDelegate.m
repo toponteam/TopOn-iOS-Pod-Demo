2f03aa09c97b7726ff74b9f6e2ed7387ecb4bb6c
ling <tianzi.ling@toponad.com>
2025-08-20 17:50:30 +0800

    remove scene

--- iOSDemo/iOSDemo/AppDelegate.m
+++ iOSDemo/iOSDemo/AppDelegate.m
@@ -24,64 +24,44 @@
     //开启Demo日志打印
     DemoLogAccess(1);
  
-    // iOS 13以下版本仍使用传统方式
-    if (@available(iOS 13.0, *)) {
-        // iOS 13及以上版本使用Scene Delegate
-    } else {
-        //布局demoUI,无需接入
-        [self setupDemoUI];
-        
-        //开屏广告展示启动图
-        [[AdSDKManager sharedManager] addLaunchLoadingView];
-        //初始化SDK，在非欧盟地区发行的应用，需要用此方法初始化SDK接入，欧盟地区初始化替换为[[AdSDKManager sharedManager] initSDK_EU:];
-        [[AdSDKManager sharedManager] initSDK];
-        //初始化广告SDK完成
-        
-        //加载开屏广告
-        [[AdSDKManager sharedManager] loadSplashAdWithPlacementID:FirstAppOpen_PlacementID result:^(BOOL isSuccess) {
-            //加载成功
-            if (isSuccess) {
-                //展示开屏广告
-                [[AdSDKManager sharedManager] showSplashWithPlacementID:FirstAppOpen_PlacementID];
-            }
-        }];
-        
-        //含欧盟地区初始化流程
-    //    //欧盟地区初始化替换为[[AdSDKManager sharedManager] initSDK_EU:];
-    //    [[AdSDKManager sharedManager] initSDK_EU:^{
-    //        //初始化广告SDK完成
-    //
-    //        //加载开屏广告
-    //        [[AdSDKManager sharedManager] loadSplashAdWithPlacementID:FirstAppOpen_PlacementID result:^(BOOL isSuccess) {
-    //            //加载成功
-    //            if (isSuccess) {
-    //                //展示开屏广告
-    //                [[AdSDKManager sharedManager] showSplashWithPlacementID:FirstAppOpen_PlacementID];
-    //            }
-    //        }];
-    //    }];
-    }
+    //布局demoUI,无需接入
+    [self setupDemoUI];
+    
+    //开屏广告展示启动图
+    [[AdSDKManager sharedManager] addLaunchLoadingView];
+    //初始化SDK，在非欧盟地区发行的应用，需要用此方法初始化SDK接入，欧盟地区初始化替换为[[AdSDKManager sharedManager] initSDK_EU:];
+    [[AdSDKManager sharedManager] initSDK];
+    //初始化广告SDK完成
+    
+    //加载开屏广告
+    [[AdSDKManager sharedManager] loadSplashAdWithPlacementID:FirstAppOpen_PlacementID result:^(BOOL isSuccess) {
+        //加载成功
+        if (isSuccess) {
+            //展示开屏广告
+            [[AdSDKManager sharedManager] showSplashWithPlacementID:FirstAppOpen_PlacementID];
+        }
+    }];
+    
+    //含欧盟地区初始化流程
+//    //欧盟地区初始化替换为[[AdSDKManager sharedManager] initSDK_EU:];
+//    [[AdSDKManager sharedManager] initSDK_EU:^{
+//        //初始化广告SDK完成
+//
+//        //加载开屏广告
+//        [[AdSDKManager sharedManager] loadSplashAdWithPlacementID:FirstAppOpen_PlacementID result:^(BOOL isSuccess) {
+//            //加载成功
+//            if (isSuccess) {
+//                //展示开屏广告
+//                [[AdSDKManager sharedManager] showSplashWithPlacementID:FirstAppOpen_PlacementID];
+//            }
+//        }];
+//    }];
        
     return YES;
 }
  
-#pragma mark - UISceneSession lifecycle
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options API_AVAILABLE(ios(13.0)) {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    UISceneConfiguration *configuration = [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-    configuration.delegateClass = NSClassFromString(@"SceneDelegate");
-    return configuration;
-}
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions API_AVAILABLE(ios(13.0)) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
- 
-- (void)applicationDidBecomeActive:(UIApplication *)application { 
+#pragma mark - lifecycle
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         if (@available(iOS 14, *)) {
