6755e6afedfb125de8cb7aa6e019bbb770f1f108
ling <>
2026-03-30 16:01:04 +0800

    Updated to v6.5.43

--- OCDemo_v6.5.xx/iOSDemo/AdDemo/Splash/SplashVC.m
+++ OCDemo_v6.5.xx/iOSDemo/AdDemo/Splash/SplashVC.m
@@ -196,6 +196,16 @@
     
     // Enter home page
     [self enterHomeVC];
+     
+    // Get close reason here: dismiss_type
+//    typedef NS_OPTIONS(NSInteger, ATAdCloseType) {
+//        ATAdCloseUnknow = 1,            // ad close type unknow
+//        ATAdCloseSkip = 2,              // ad skip to close
+//        ATAdCloseCountdown = 3,         // ad countdown to close
+//        ATAdCloseClickcontent = 4,      // ad clickcontent to close
+//        ATAdCloseShowfail = 99          // ad showfail to close
+//    };
+//    ATAdCloseType closeType = [extra[kATADDelegateExtraDismissTypeKey] integerValue];
     
     // Pre-load for hot launch (optional)
     // [self loadAd];
